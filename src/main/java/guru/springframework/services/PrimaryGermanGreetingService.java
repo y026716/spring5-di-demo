79b758ff719ece44dfbd79dd9e801c8e756e3f48
JBoysen <y026716@gmail.com>
2020-10-03 21:38:29 -0500

    Java based Spring Configuration for Service Factory

--- src/main/java/guru/springframework/services/PrimaryGermanGreetingService.java
+++ src/main/java/guru/springframework/services/PrimaryGermanGreetingService.java
@@ -1,4 +1,4 @@
-package guru.services;
+package guru.springframework.services;
 
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
@@ -7,19 +7,17 @@ import org.springframework.stereotype.Service;
 /**
  * Created by jt on 5/24/17.
  */
-@Service
-@Primary
-@Profile({"en", "default"})
-public class PrimaryGreetingService implements GreetingService {
+
+public class PrimaryGermanGreetingService implements GreetingService {
 
     private GreetingRepository greetingRepository;
 
-    public PrimaryGreetingService(GreetingRepository greetingRepository) {
+    public PrimaryGermanGreetingService(GreetingRepository greetingRepository) {
         this.greetingRepository = greetingRepository;
     }
 
     @Override
     public String sayGreeting() {
-        return greetingRepository.getEnglishGreeting();
+        return greetingRepository.getGermanGreeting();
     }
 }
--- src/main/java/guru/springframework/services/PrimarySpanishGreetingService.java
+++ src/main/java/guru/springframework/services/PrimarySpanishGreetingService.java
@@ -1,4 +1,4 @@
-package guru.services;
+package guru.springframework.services;
 
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
@@ -7,19 +7,17 @@ import org.springframework.stereotype.Service;
 /**
  * Created by jt on 5/24/17.
  */
-@Service
-@Primary
-@Profile("de")
-public class PrimaryGermanGreetingService implements GreetingService {
+
+public class PrimarySpanishGreetingService implements GreetingService {
 
     private GreetingRepository greetingRepository;
 
-    public PrimaryGermanGreetingService(GreetingRepository greetingRepository) {
+    public PrimarySpanishGreetingService(GreetingRepository greetingRepository) {
         this.greetingRepository = greetingRepository;
     }
 
     @Override
     public String sayGreeting() {
-        return greetingRepository.getGermanGreeting();
+        return greetingRepository.getSpanishGreeting();
     }
 }
